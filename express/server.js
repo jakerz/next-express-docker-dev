04879b74a7e9388316f4c99c0aa3dbb1b24da8e6
Jake Ramirez <jake2ramirez@gmail.com>
2022-10-17 06:49:07 +0000

    Modified new express server.js

--- express/server.js
+++ express/server.js
@@ -1,23 +1,22 @@
 'use strict';
 const express = require('express');
+const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
-function updateDatabase(data) {
-  ... // update the database
-    return newValue;
-}
-
-// Middleware
-app.use(bodyParser);
-// Create POST endpoint at /updatestate
-app.post('/updatestate', (req, res) => {
-    const newValue = updateDatabase(res.body);
-    // Respond with JSON valuee returned from DB update fn
-    res.json(newValue);
+const router = express.Router();
+router.get('/', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Hello from Express.js!</h1>');
+    res.end();
 });
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-// Serverless http is converting our express app above
-// into something lambda functions can use
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+module.exports = app;
 module.exports.handler = serverless(app);
