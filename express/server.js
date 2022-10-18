39ffcc3d50da7bbebd66faff230cca153e91b990
Jake Ramirez <jake2ramirez@gmail.com>
2022-10-18 08:29:27 +0000

    Moved

--- express/server.js
+++ express/server.js
@@ -1,5 +1,6 @@
 'use strict';
 const express = require('express');
+const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
@@ -14,9 +15,8 @@ router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-// app.use('/', (req, res) => res.sendFile(path.join(__dirname, '/.next/index.html')));
+app.use('/netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
-
