4350cabe85ead1df905f3ca7feab6e2ccbc1176b
Jake Ramirez <jake2ramirez@gmail.com>
2022-10-18 08:02:59 +0000

    Changes to html, server.js

--- express/server.js
+++ express/server.js
@@ -1,6 +1,5 @@
 'use strict';
 const express = require('express');
-const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
@@ -16,7 +15,8 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-// app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../.next/index.html')));
+// app.use('/', (req, res) => res.sendFile(path.join(__dirname, '/.next/index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
+
